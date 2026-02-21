2cd701106c88cce49ff72f18d5c39e4d470c5f5a
STONE <17th@inbox.ru>
2026-02-21 16:07:33 +0300

    upd: routers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -9,10 +9,11 @@ from .user_router import user_router
 bot_main_router = Router()
 
 bot_main_router.include_routers(
+fsm_router,
 admin_router,
 start_router,
 user_router,
-    fsm_router,
+
 
 
 
